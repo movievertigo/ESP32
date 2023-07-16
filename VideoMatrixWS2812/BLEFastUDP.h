07734ef499ace12e295fa765a4fe364ef628806c
Movie Vertigo <movievertigo@gmail.com>
2023-07-16 17:38:46 +0100

    Initial commit

---
+++ VideoMatrixWS2812/BLEFastUDP.h
@@ -0,0 +1,65 @@
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLE2902.h>
+
+class BLEFastUDP : public BLECharacteristicCallbacks, public BLEServerCallbacks
+{
+public:
+  typedef void (*RXCallbackFunction)(const uint8_t*, int);
+
+  void begin(const std::string& name, const std::string& info, const std::string& serviceUUID, RXCallbackFunction rxCallback)
+  {
+    ServiceUUID = serviceUUID;
+    InfoUUID    = MakeCharacteristicUUID(serviceUUID, 1);
+    RXUUID      = MakeCharacteristicUUID(serviceUUID, 3);
+    RXCallback  = rxCallback;
+
+  	BLEDevice::init(name);
+	  BLEServer* server = BLEDevice::createServer();
+    server->setCallbacks(this);
+
+    BLEService* service = server->createService(ServiceUUID);
+  	service->createCharacteristic(    InfoUUID, BLECharacteristic::PROPERTY_READ  )->setValue(info);
+    service->createCharacteristic(      RXUUID, BLECharacteristic::PROPERTY_WRITE )->setCallbacks(this);
+  	service->start();
+
+	  BLEAdvertising* advertising = BLEDevice::getAdvertising();
+	  advertising->addServiceUUID(ServiceUUID);
+	  advertising->start();
+  }
+
+private:
+  void onConnect(BLEServer* server, esp_ble_gatts_cb_param_t* param)
+  {
+    Serial.print("Bluetooth connected\n");
+    server->updateConnParams(param->connect.remote_bda, 15, 15, 0, param->connect.conn_params.timeout);
+    server->startAdvertising();
+  }
+
+  void onDisconnect(BLEServer* server)
+  {
+    Serial.print("Bluetooth disconnected\n");
+	  server->startAdvertising();
+  }
+
+  void onWrite(BLECharacteristic* characteristic)
+  {
+	  if (characteristic->getUUID().toString() == RXUUID)
+  	{
+      std::string value = characteristic->getValue();
+      RXCallback((const uint8_t*)value.data(), value.length());
+	  }
+  }
+
+  std::string MakeCharacteristicUUID(const std::string& serviceUUID, int offset)
+  {
+    char end[9];
+    sprintf(end, "%08x", std::stoul(serviceUUID.substr(serviceUUID.length()-8), nullptr, 16) + offset);
+    return serviceUUID.substr(0, serviceUUID.length()-8) + end;
+  }
+
+  std::string ServiceUUID;
+  std::string InfoUUID;
+  std::string RXUUID;
+  RXCallbackFunction RXCallback;
+};
---
+++ VideoMatrixWS2812/VideoMatrixWS2812.ino
@@ -0,0 +1,64 @@
+#include <esp_task_wdt.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_NeoMatrix.h>
+#include <Adafruit_NeoPixel.h>
+#include "BLEFastUDP.h"
+
+const int MatrixBrightness = 32;
+const int MatrixTileWidth = 8;
+const int MatrixTileHeight = 8;
+const int MatrixTileCountX = 2;
+const int MatrixTileCountY = 2;
+
+const std::string SERVICE_UUID = "5669644d-4d6f-7669-6556-65727469676f"; // VidM-Mo-vi-eV-ertigo
+
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(MatrixTileWidth, MatrixTileHeight, MatrixTileCountX, MatrixTileCountY, 2,
+  NEO_TILE_TOP       + NEO_TILE_RIGHT +
+  NEO_TILE_COLUMNS   + NEO_TILE_PROGRESSIVE +
+  NEO_MATRIX_TOP     + NEO_MATRIX_RIGHT +
+  NEO_MATRIX_COLUMNS + NEO_MATRIX_PROGRESSIVE,
+  NEO_GRB);
+
+BLEFastUDP bleData;
+const int MatrixWidth = MatrixTileWidth * MatrixTileCountX;
+const int MatrixHeight = MatrixTileHeight * MatrixTileCountY;
+const int MatrixDataLength = MatrixWidth * MatrixHeight * 2;
+
+void setup()
+{
+  Serial.begin(115200);
+
+  matrix.begin();
+  matrix.setBrightness(MatrixBrightness);
+
+  std::string info = "{\"width\":"+std::to_string(MatrixWidth)+",\"height\":"+std::to_string(MatrixHeight)+"}";
+
+  esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT);
+  bleData.begin("Video Matrix", info, SERVICE_UUID, processData);
+
+  disableCore0WDT();
+  disableCore1WDT();
+  disableLoopWDT();
+  esp_task_wdt_delete(NULL);
+}
+
+void processData(const uint8_t* data, int length)
+{
+  if (length == MatrixDataLength)
+  {
+    const uint16_t* pixelData = (const uint16_t*)data;
+
+    for (int y = 0; y < MatrixHeight; ++y)
+    {
+      for (int x = 0; x < MatrixWidth; ++x)
+      {
+        matrix.drawPixel(x, y, *pixelData++);
+      }
+    }
+  }
+}
+
+void loop()
+{
+      matrix.show();
+}
